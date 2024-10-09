85bf457618a6faa72335bec54254ecb9fdede69c
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-10-09 12:22:27 +0530

    Added: SuSumOfNaturalNumber

---
+++ src/main/java/com/bridgelabz/loopingprogram/SumOfNaturalNumber.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.loopingprogram;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumber {
+    public static void main(String[] args) {
+        int n=5;
+        int sum =0;
+        int i = 1;
+
+        while(i <= n){
+            sum += i;
+            i++;
+
+
+         }
+          System.out.println("Sum Of Natural Number is "+n+ "is: "  +sum );
+
+    }
+}
