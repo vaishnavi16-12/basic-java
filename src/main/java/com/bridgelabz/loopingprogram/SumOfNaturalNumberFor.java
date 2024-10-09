f3057a6f6e5c1aded1160db9cf981b3fed8b7fe0
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-10-10 00:43:35 +0530

    Added: Sum of natural numbaer using for loop

---
+++ src/main/java/com/bridgelabz/loopingprogram/SumOfNaturalNumberFor.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.loopingprogram;
+
+public class SumOfNaturalNumberFor {
+    public static void main(String[] args) {
+        int n=5;
+         int sum=0;
+
+
+        for(int i=1; i<=n; i++){
+            sum += i;
+
+
+        }
+        System.out.println("Sum Of Natural number "+n+ "is: "+ sum);
+    }
+}
