aadcf2c0a48408e7c72b6d574e0d3d3c818b8019
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-10-09 13:12:25 +0530

    Added: Reverse Number

---
+++ src/main/java/com/bridgelabz/loopingprogram/ReverseIntegerNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.loopingprogram;
+
+import java.util.Scanner;
+
+public class ReverseIntegerNumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Integer Number: ");
+        int n = sc.nextInt();
+        int  reverse=0;
+
+        while (n != 0){
+            int  rem = n%10;
+            reverse = reverse * 10+ rem;
+             n=n/10;
+
+
+
+        }
+  System.out.println("Reversed Number: " + reverse);
+    }
+}
